db76f5b34bd0e4ecc9bf0715419f7c7910923d66
optik9 <choyoss@gmail.com>
2025-03-07 09:18:40 -0500

    version 2.3

--- src/app.js
+++ src/app.js
@@ -32,3 +32,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
+
+//module.exports = app;
+
+export default app;
